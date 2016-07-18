59a85b6aefa679bae2c4550b4b728832dbff2df4
Luter <lutersergei@mail.ru>
2016-07-18 14:40:46 +0700

    ЧПУ

--- controller/users.controller.php
+++ controller/users.controller.php
@@ -1,10 +1,10 @@
 <?php
 class UsersController extends Controller
 {
-  public function __call($name, $arguments)
-  {
-    die($name);
-  }
+    public function __call($name, $arguments)
+    {
+        die($name);
+    }
 
     public function __construct()
     {
@@ -16,31 +16,31 @@ class UsersController extends Controller
 //        }
     }
 
-  public function users_index()
-  {
-    $users = Users::all();
-
-    return $this->render("users/users_list", ['users' => $users]);
-  }
-
-  public function users_add()
-  {
-    if (isset($_POST['__action']))
+    public function users_index()
     {
-      if ($_POST['__action'] === 'add')
-      {
-          $password = $_POST['__password'];
-          $users = new Users();
-          $users->load(System::post());
-          $users->create_password($password);
-          $result = $users->add();
-          if (System::create_message('add',$result))
-          {
-              header('Location:/users/index');
-              die();
-          }
-      }
+        $users = Users::all();
+
+        return $this->render("users/users_list", ['users' => $users]);
     }
-    return $this->render("users/users_add", []);
-  }
+
+    public function users_add()
+    {
+        if (isset($_POST['__action']))
+        {
+            if ($_POST['__action'] === 'add')
+            {
+                $password = $_POST['__password'];
+                $users = new Users();
+                $users->load(System::post());
+                $users->create_password($password);
+                $result = $users->add();
+                if (System::create_message('add',$result))
+                {
+                    header('Location:/users/index');
+                    die();
+                }
+            }
+        }
+        return $this->render("users/users_add", []);
+    }
 }
