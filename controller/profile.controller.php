c2fef946298f795be62658dc6f6fb2d9c2c57f43
Luter <lutersergei@mail.ru>
2016-07-21 22:17:49 +0700

    controller avatar

--- controller/profile.controller.php
+++ controller/profile.controller.php
@@ -24,6 +24,8 @@ class ProfileController extends Controller
 
     public function profile_edit($id)
     {
+        global $alphabet;
+
         if ($id === NULL)
         {
             header('Location:/users/index');
@@ -38,6 +40,62 @@ class ProfileController extends Controller
             if ($_POST['__action'] === 'edit')
             {
                 $profile = new Profile($id);
+
+                if (isset($_FILES['avatar']))
+                {
+                    if ($_FILES['avatar']['error'] === 0)
+                    {
+                        $type_array=getimagesize($_FILES['avatar']['tmp_name']);
+                        $file_name=$_FILES['avatar']['name'];
+                        $file_translit_name=TranslitHelper::transliteration($file_name,$alphabet);
+                        $file_path=UPLOAD_IMAGES_FOLDER.$file_translit_name;
+                        $file_thumb_path=THUMBNAILS_FOLDER.$file_translit_name;
+                        $file_temp_name=$_FILES['avatar']['tmp_name'];
+                        $file_size=$_FILES['avatar']['size'];
+                        $file_error=$_FILES['avatar']['error'];
+                        if (($file_size)<MAX_SIZE)
+                        {   //Проверка размера файла
+                            if (($type_array[2])===(1)||($type_array[2])===(2)||($type_array[2])===(3))
+                            {
+                                var_dump($file_error);
+                                if (($file_error)===0)
+                                {
+                                    move_uploaded_file($file_temp_name, $file_path);
+                                    ThumbnailsHelper::makeThumbnails(THUMBNAILS_FOLDER ,$file_path, $file_translit_name); //TODO Сделать квадратные превьюшки
+                                    $profile->avatar = $file_thumb_path;
+                                    $result = $profile->update();
+                                    if (System::create_message('update',$result))
+                                    {
+                                        header("Location:/users/profile/$users->id");
+                                        die();
+                                    }
+                                }
+                                else
+                                {
+                                    System::set_message('error', ERROR);
+                                    header("Location:/users/profile/$users->id");
+                                    die();
+                                }
+                            }
+                            else
+                            {
+                                System::set_message('error', DANGER_NOT_IMAGE);
+                                header("Location:/users/profile/$users->id");
+                                die();
+                            }
+                        }
+                        else
+                        {
+                            System::set_message('error', DANGER_SIZE_EXCEEDED);
+                            header("Location:/users/profile/$users->id");
+                            die();
+                        }
+                    }
+                    else
+                    {
+                        //TODO Сообщение: не выбрали файл для фото
+                    }
+                }
                 $profile->load(System::post());
                 $result = $profile->update();
                 if (System::create_message('update',$result))
